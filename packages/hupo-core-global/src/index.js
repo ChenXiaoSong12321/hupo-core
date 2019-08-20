aae94e23b366bf23ad5fe6783314fe083c690bbc
卢逸平 <372254032@qq.com>
2019-08-20 18:49:04 +0800

    修改global存储地方

--- packages/hupo-core-global/src/index.js
+++ packages/hupo-core-global/src/index.js
@@ -1,4 +1,12 @@
 // hupo
-
-export const _global = typeof window === 'undefined' ? global : window
+const getGlobal = () => {
+  if(typeof window === 'undefined'){
+    if(!global.$mall)global.$mall = {}
+    return global.$mall
+  }else{
+    if(!window.$mall)window.$mall = {}
+    return window.$mall
+  }
+}
+export const _global = getGlobal()
 export default _global
