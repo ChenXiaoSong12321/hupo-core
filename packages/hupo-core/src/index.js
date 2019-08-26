83d8d74225ae738802a4bc94565a6a6a0009f59a
卢逸平 <372254032@qq.com>
2019-08-26 17:47:34 +0800

    core add other package

--- packages/hupo-core/src/index.js
+++ packages/hupo-core/src/index.js
@@ -1,6 +1,9 @@
 export { default as global} from "@hupo/core-global";
 export { default as promise} from "@hupo/core-promise";
 export { default as wxTools} from "@hupo/core-wx-app-tools";
+export { componentBaseTreeMixin, pageBaseTreeMixin} from "@hupo/core-cml-base-tree";
+export { Event, componentEventMixin, pageEventMixin} from "@hupo/core-cml-event";
+export { default as dayjs} from "@hupo/core-dayjs";
 
 export const autoRequire2object = modulesFiles => {
   const modules = modulesFiles.keys().reduce((modules, modulePath) => {
