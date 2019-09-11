8fbc6e353119c83689be2d21e2eaac50815b8720
卢逸平 <372254032@qq.com>
2019-09-11 17:22:17 +0800

    build

--- packages/hupo-core/pkg/dist-node/index.js
+++ packages/hupo-core/pkg/dist-node/index.js
@@ -4,6 +4,8 @@ Object.defineProperty(exports, '__esModule', { value: true });
 
 function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
 
+var coreRequestWeb = require('@hupo/core-request-web');
+var coreRequestWxApp = require('@hupo/core-request-wx-app');
 var coreGlobal = _interopDefault(require('@hupo/core-global'));
 var corePromise = _interopDefault(require('@hupo/core-promise'));
 var coreWxAppTools = _interopDefault(require('@hupo/core-wx-app-tools'));
@@ -13,48 +15,53 @@ var coreDayjs = _interopDefault(require('@hupo/core-dayjs'));
 var coreTimer = _interopDefault(require('@hupo/core-timer'));
 var coreUrl = _interopDefault(require('@hupo/core-url'));
 var coreRequestBase = _interopDefault(require('@hupo/core-request-base'));
-var coreRequestWeb = _interopDefault(require('@hupo/core-request-web'));
-var coreRequestWxApp = _interopDefault(require('@hupo/core-request-wx-app'));
 
-
+const requestWxapp = {
+  request: coreRequestWxApp.request,
+  initialize: coreRequestWxApp.initialize
+};
+const requestWeb = {
+  request: coreRequestWeb.request,
+  initialize: coreRequestWeb.initialize
+};
 
 exports.global = coreGlobal;
 exports.promise = corePromise;
 exports.wxTools = coreWxAppTools;
 Object.defineProperty(exports, 'componentBaseTreeMixin', {
-	enumerable: true,
-	get: function () {
-		return coreCmlBaseTree.componentBaseTreeMixin;
-	}
+  enumerable: true,
+  get: function () {
+    return coreCmlBaseTree.componentBaseTreeMixin;
+  }
 });
 Object.defineProperty(exports, 'pageBaseTreeMixin', {
-	enumerable: true,
-	get: function () {
-		return coreCmlBaseTree.pageBaseTreeMixin;
-	}
+  enumerable: true,
+  get: function () {
+    return coreCmlBaseTree.pageBaseTreeMixin;
+  }
 });
 Object.defineProperty(exports, 'Event', {
-	enumerable: true,
-	get: function () {
-		return coreCmlEvent.Event;
-	}
+  enumerable: true,
+  get: function () {
+    return coreCmlEvent.Event;
+  }
 });
 Object.defineProperty(exports, 'componentEventMixin', {
-	enumerable: true,
-	get: function () {
-		return coreCmlEvent.componentEventMixin;
-	}
+  enumerable: true,
+  get: function () {
+    return coreCmlEvent.componentEventMixin;
+  }
 });
 Object.defineProperty(exports, 'pageEventMixin', {
-	enumerable: true,
-	get: function () {
-		return coreCmlEvent.pageEventMixin;
-	}
+  enumerable: true,
+  get: function () {
+    return coreCmlEvent.pageEventMixin;
+  }
 });
 exports.dayjs = coreDayjs;
 exports.timer = coreTimer;
 exports.url = coreUrl;
 exports.requestBase = coreRequestBase;
-exports.requestWeb = coreRequestWeb;
-exports.requestWxapp = coreRequestWxApp;
+exports.requestWeb = requestWeb;
+exports.requestWxapp = requestWxapp;
 //# sourceMappingURL=index.js.map
--- packages/hupo-core/pkg/dist-src/index.js
+++ packages/hupo-core/pkg/dist-src/index.js
@@ -1,3 +1,5 @@
+import { request as requestWeb$request, initialize as requestWeb$initialize } from "@hupo/core-request-web";
+import { request as requestWxapp$request, initialize as requestWxapp$initialize } from "@hupo/core-request-wx-app";
 export { default as global } from "@hupo/core-global";
 export { default as promise } from "@hupo/core-promise";
 export { default as wxTools } from "@hupo/core-wx-app-tools";
@@ -7,5 +9,11 @@ export { default as dayjs } from "@hupo/core-dayjs";
 export { default as timer } from "@hupo/core-timer";
 export { default as url } from "@hupo/core-url";
 export { default as requestBase } from "@hupo/core-request-base";
-export { default as requestWeb } from "@hupo/core-request-web";
-export { default as requestWxapp } from "@hupo/core-request-wx-app";
+export const requestWxapp = {
+  request: requestWxapp$request,
+  initialize: requestWxapp$initialize
+};
+export const requestWeb = {
+  request: requestWeb$request,
+  initialize: requestWeb$initialize
+};
