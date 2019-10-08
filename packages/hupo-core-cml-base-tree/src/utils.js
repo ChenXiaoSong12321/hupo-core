1ab319218702f04aca338df04531514fb336962f
卢逸平 <372254032@qq.com>
2019-10-08 10:03:22 +0800

    优化component _page生成

--- packages/hupo-core-cml-base-tree/src/utils.js
+++ packages/hupo-core-cml-base-tree/src/utils.js
@@ -1,2 +1,2 @@
-export const getViewId = instance => instance.__wxWebviewId__ || instance._uid
+export const getViewId = instance => instance._uid ? instance._uid : (instance.__wxWebviewId__ || instance.getPageId())
 export const getComponentName = instance => instance.__cml_originOptions__ ? instance.__cml_originOptions__.name : instance.$options.name
