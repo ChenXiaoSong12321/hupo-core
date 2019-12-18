1e9fa97044c6d1924469705deae7643999ad2601
卢逸平 <372254032@qq.com>
2019-12-18 11:56:19 +0800

    request&细节调整

---
+++ packages/hupo-core-request-uni/pkg/dist-src/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+module.exports = TODO;
+
+function TODO() {// TODO
+}
---
+++ packages/hupo-core-request-uni/src/index.js
@@ -0,0 +1,45 @@
+import * as requestUtils from '@hupo/core-request-utils'
+import { createError } from '@hupo/core-promise'
+import { global } from '@hupo/core-global'
+
+const CONFIG = requestUtils.defaultConfig()
+
+const toUniConfig = config => {
+  const { options, data = {}, params = {}, headers = CONFIG.headers, timeout = CONFIG.timeout, setting = {}} = config
+  const urlParams = requestUtils.formatParams(params)
+  return {
+    url: `${global._request.baseUrl}${options.url}${urlParams}`,
+    data,
+    header: headers,
+    method: options.type.toUpperCase(),
+    timeout
+  }
+}
+
+export const send = async config => {
+  const uniConfig = toUniConfig(config)
+  const task = uni.request(uniConfig)
+  await requestUtils.pendding(config)
+  const [error, res] = await task
+  requestUtils.removePending(config)
+  if (error) {
+    return Promise.reject(createError({
+      message: 'request fail',
+      info: error,
+      response: null
+    }))
+  }
+  const response = {
+    config,
+    status: res.statusCode,
+    data: res.data
+  }
+  await requestUtils.filterError(response)
+  requestUtils.filterResponse(response)
+  return requestUtils.complete(response)
+}
+
+export const request = {
+  initialize: requestUtils.initialize,
+  send
+}
