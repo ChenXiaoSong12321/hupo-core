128cfaab6c580e64379cc795845ab5f9b419b595
卢逸平 <372254032@qq.com>
2019-12-24 17:29:11 +0800

    viewport增加api

--- packages/hupo-core-viewport/src/index.js
+++ packages/hupo-core-viewport/src/index.js
@@ -10,20 +10,24 @@ const calculate = () => {
     return calculate()
   }
   data.pixelRatio = system.pixelRatio
-  data.rpxRatio = 750 / system.screenWidth
+  const rpxRatio = 750 / system.screenWidth
+
+  const rpx2px = rpx => rpx / rpxRatio
+  const px2rpx = rpx => rpx * rpxRatio
+
   // #ifdef MP-WEIXIN
   if (system.platform == 'devtools') {
-    data.capsuleHeight = 44 * data.rpxRatio
+    data.capsuleHeight = px2rpx(44)
   } else if (system.platform == 'android') {
-    data.capsuleHeight = 48 * data.rpxRatio
+    data.capsuleHeight = px2rpx(48)
   }
-  data.statusBarHeight = system.statusBarHeight * data.rpxRatio
+  data.statusBarHeight = px2rpx(system.statusBarHeight)
   if (system.screenHeight - data.statusBarHeight > 750 && system.platform != 'android') data.isAllScreen = true
   // #endif
   // #ifdef H5
   const isAllScreen = () => /iphone/gi.test(window.navigator.userAgent) && window.screen.height >= 812
   if (isAllScreen()) data.isAllScreen = true
-  data.capsuleHeight = data.capsuleHeight * data.rpxRatio
+  data.capsuleHeight = px2rpx(data.capsuleHeight)
   if (channel === channels.WX_H5) {
     data.capsuleHeight = 0
   }
@@ -33,7 +37,9 @@ const calculate = () => {
   }
   data.headerHeight = data.statusBarHeight + data.capsuleHeight
   // 全面屏 底部留空距离 34px
-  data.bottomHeight = data.isAllScreen ? 34 * data.rpxRatio : 0
+  data.bottomHeight = data.isAllScreen ? px2rpx(34) : 0
+  data.rpx2px = rpx2px
+  data.px2rpx = px2rpx
   return data
 }
 export const viewport = calculate()
